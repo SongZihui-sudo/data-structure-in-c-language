4c97090c90949fb1a481b9e3aa18cad107ab2e7e
宋子慧 <77034643+SongZihui-sudo@users.noreply.github.com>
2021-09-26 10:13:59 +0800

    Update Bitmap.c

--- Bitmap.c
+++ Bitmap.c
@@ -13,8 +13,6 @@ struct bitmap *creat_bitmap(); //创建bitmap
 //结构体
 typedef struct bitmap{
     int key[MAXSIZE]; //原始数据
-    int bit[8][8]; //bit
-    int sort[MAXSIZE]; //排序后数据
     int bitmap[mapsize]; //bitmap
 }bitmap;
 /*
@@ -41,14 +39,13 @@ struct bitmap *input(bitmap *b){
     int l;
     for (int i = 0; i < MAXSIZE; i++){
         b->key[i]=rand() % 51 + 13 ; 
-        b->sort[i]=b->key[i];    
     }
     for (int j = 0; j < MAXSIZE - 1; j++){
         for (int k = 0; k < MAXSIZE - 1 -j; k++){
-            if (b->sort[k]>b->sort[k+1]){
-                l=b->sort[k];
-                b->sort[k]=b->sort[k+1];
-                b->sort[k+1]=l;    
+            if (b->key[k]>b->key[k+1]){
+                l=b->key[k];
+                b->key[k]=b->key[k+1];
+                b->key[k+1]=l;    
             } 
         }
     }
@@ -59,41 +56,19 @@ struct bitmap *creat_bitmap(bitmap *creat_bit){
     int h,w,j;
     uint bit;
     bit=0200;
+    for (int i = 0; i < 8; i++){
+        creat_bit->bitmap[i] = 0;
+    }
+    
     for (j = 0; j < MAXSIZE; j++){
-        w = creat_bit->sort[j];        
+        w = creat_bit->key[j];        
         while (w >= 0){
             w=w-8;
         }                      //根据数值得到相应的bit
         w=w+8;
         h=-(-8+w-j)/8;
-        creat_bit->bit[h][w]=bit>>w;
+        creat_bit->bitmap[h]= creat_bit->bitmap[h] | bit>>w;
     }        
-    //创建bitmap
-    for (int i = 0; i < MAXSIZE/8; i++){
-        j=0;
-        while (creat_bit->bit[i][j] < 0){
-                j++;
-            } //防止填充数影响结果
-        bit=creat_bit->bit[i][j];
-        for (int k = 0; k < 8; k++){
-            while (creat_bit->bit[i][k] < 0){
-                k++; 
-            }
-            bit= bit | creat_bit->bit[i][k]; //通过或得到这一列的bit值    
-        }
-        creat_bit->bitmap[i]=bit;
-        
-    }
     return creat_bit; //返回指针
 }
-/*
-    creat map:
-    1 0 0 0 0 1 1 1
-    0 0 1 1 1 1 0 0
-    0 1 1 1 1 1 0 1
-    0 0 0 1 1 0 1 1
-    0 0 0 0 1 1 1 1
-    1 0 1 0 0 0 1 1
-    0 1 0 0 0 1 1 0 
-    0 0 1 0 1 1 0 1
-*/
+
